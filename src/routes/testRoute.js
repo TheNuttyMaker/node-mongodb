c2169b99dcc07f736b4183714b3167d9882d1b16
Prashant Yadav <pygold@gmail.com>
2023-04-18 00:41:11 +0530

    Update testRoute.js. Added default route

--- src/routes/testRoute.js
+++ src/routes/testRoute.js
@@ -5,3 +5,12 @@ export const testRoute = {
     res.status(200).send("It works!");
   },
 };
+
+export const defaultRoute = {
+  path: "/",
+  method: "get",
+  handler: (req, res) => {
+    res.status(200).send("Welcome! The Node server works. Check Readme to see how to use other API endpoints");
+  },
+};
+    
